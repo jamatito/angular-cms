862f4e4e82ea90b63e20883c2709f5e773fbcf93
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-03-21 11:20:19 +0100

    Configurando rutas.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,14 +2,17 @@ import {ModuleWithProviders} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 
 
 const  appRoutes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'inicio', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: 'inicio', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'registro', component: RegisterComponent}
+  {path: 'signup', component: RegisterComponent},
+  {path: '**', component: ErrorComponent}
 
 ];
 
