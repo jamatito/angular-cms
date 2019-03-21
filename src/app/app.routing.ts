266d65b37ad78f0f54045cad11a515ab7f54f2c8
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-03-21 10:39:37 +0100

    Creando componentes login y register. Usando ng g component components/register

---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import {ModuleWithProviders} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+
+
+
+const  appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'inicio', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegisterComponent}
+
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
