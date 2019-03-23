9a91dc67b5eb4993690e8dd06596c9947b0a1c5b
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-03-23 10:35:20 +0100

    Haciendo un formulario con las funciones de angular ng-form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -8,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   public pageTitle: string;
+  public user: User;
 
   constructor() {
     this.pageTitle = 'Registrarse';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+    /*      public id: number,
+      public name: string,
+      public surname: string,
+      public role: string,
+      public email: string,
+      public password: string,
+      public description: string,
+      public image: string*/
+
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form) {
+    console.log(this.user);
+    form.reset();
   }
 
 }
