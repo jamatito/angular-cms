1ebc6aedde4334fd33dc1eed009dedc462ee00fc
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-03-31 14:56:38 +0200

    Fallo al hacer la petición AJAX de update.

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  public pageTitle: string;
+  public user: User;
+  public status: string;
+  public token;
+  public identity;
+
+  constructor(private userService: UserService) {
+    this.pageTitle = 'Editar perfil';
+    // this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+    this.identity = this.userService.getIdentity();
+    this.token = this.userService.getToken();
+    this.user = this.identity;
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form) {
+    this.userService.update(this.user, this.token).subscribe(
+      response => {
+        /* if (response.status != 'error') {
+           this.status = 'success';
+           this.token = response;
+         } else {
+           this.status = 'error';
+         }*/
+        console.log(response);
+      }, error => {
+        console.log(<any> error);
+        this.status = 'error';
+      }
+    );
   }
 
 }
