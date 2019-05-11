73ae4cf0ce64b1dccf9326314ce2380e19e46ac8
Juan Angel Matito <jmatitosilvero@gmail.com>
2019-05-11 11:26:53 +0200

    Instalando módulo ng4-social-login, para iniciar sesión con Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,28 @@ import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
 import {UserEditComponent} from './components/user-edit/user-edit.component';
 import {FroalaEditorModule, FroalaViewModule} from 'angular-froala-wysiwyg';
 import {AngularFileUploaderModule} from 'angular-file-uploader';
-import { CategoryNewComponent } from './components/category-new/category-new.component';
-import { PostNewComponent } from './components/post-new/post-new.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { PostEditComponent } from './components/post-edit/post-edit.component';
-import { CategoryDetailComponent } from './components/category-detail/category-detail.component';
+import {CategoryNewComponent} from './components/category-new/category-new.component';
+import {PostNewComponent} from './components/post-new/post-new.component';
+import {PostDetailComponent} from './components/post-detail/post-detail.component';
+import {PostEditComponent} from './components/post-edit/post-edit.component';
+import {CategoryDetailComponent} from './components/category-detail/category-detail.component';
 import {IdentityGuard} from './services/identity.guard';
 import {UserService} from './services/user.service';
-import { ProfileComponent } from './components/profile/profile.component';
-import { PostBladeComponent } from './components/post-blade/post-blade.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {PostBladeComponent} from './components/post-blade/post-blade.component';
+import {SocialLoginModule, AuthServiceConfig, GoogleLoginProvider} from 'ng4-social-login';
+
+const CONFIG = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('549954033989-geugasaauc2j56ocbe53hp2q1j2qo0oh.apps.googleusercontent.com')
+  }
+], false);
+
+export function provideConfig() {
+  return CONFIG;
+}
+
 
 @NgModule({
   declarations: [
@@ -45,12 +58,17 @@ import { PostBladeComponent } from './components/post-blade/post-blade.component
     HttpClientModule,
     FroalaEditorModule.forRoot(),
     FroalaViewModule.forRoot(),
-    AngularFileUploaderModule
+    AngularFileUploaderModule,
+    SocialLoginModule
   ],
   providers: [
     appRoutingProviders,
     IdentityGuard,
-    UserService
+    UserService,
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
   ],
   bootstrap: [AppComponent]
 })
